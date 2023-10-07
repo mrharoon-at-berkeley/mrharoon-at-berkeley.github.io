618df9a073581ebcbe0419a62281600638df54b7
mrharoon-at-berkeley <mrharoon@berkeley.edu>
2023-10-07 14:34:49 -0700

    finished contacts

--- contactpage/script.js
+++ contactpage/script.js
@@ -1,55 +1,74 @@
-alertMessage = "";
-    if (userName.value === "") {
-      alertMessage += "ERROR: Missing name. Please give a name.\n";
-      pleaseName.removeAttribute("hidden");
-    } else {
-      pleaseName.setAttribute("hidden");
-    }
-    if (userEmail.value === "") {
-      alertMessage += "ERROR: Missing email. Please give an email.\n";
-      pleaseEmail.removeAttribute("hidden");
+const validateEmail = (email) =>
+  new Promise((resolve, reject) => {
+    if (email === "") {
+      reject("ERROR: Missing email. Please give an email.");
     } else {
       const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-      if (!emailRegex.test(userEmail.value)) {
-        alertMessage += "ERROR: Invalid email. Please give a VALID email.\n";
-        pleaseName.removeAttribute("hidden");
+      if (!emailRegex.test(email)) {
+        reject("ERROR: Invalid email. Please give a VALID email.");
       } else {
-        pleaseName.setAttribute("hidden");
+        resolve();
       }
     }
-    if (userContent.value === "") {
-      alertMessage +=
-        'ERROR: Missing message. Please write something under "What\'s up?"\n';
-      pleaseMessage.removeAttribute("hidden");
+  });
+const validateName = (name) =>
+  new Promise((resolve, reject) => {
+    if (name === "") {
+      reject("ERROR: Missing name. Please give a name.");
     } else {
-      pleaseMessage.setAttribute("hidden");
+      resolve();
     }
-    if (alertMessage !== "") {
-      alert(alertMessage);
-      intro.innerText =
-        "If you'd like, just email mrharoon@berkeley.edu to contact me!";
+  });
+
+const validateMessage = (message) =>
+  new Promise((resolve, reject) => {
+    if (message === "") {
+      reject("ERROR: Missing message. Please write a message.");
     } else {
-      alert("Message Sent!");
-      location.reload();
+      resolve();
     }
+  });
 
 const submit = () => {
   const userName = document.getElementById("name");
   const userEmail = document.getElementById("email");
-  const userContent = document.getElementById("content");
+  const userMessage = document.getElementById("content");
   const sendButton = document.getElementById("sendButton");
   const intro = document.getElementById("intro");
   const pleaseName = document.getElementById("pleaseName");
   const pleaseEmail = document.getElementById("pleaseEmail");
   const pleaseMessage = document.getElementById("pleaseMessage");
 
-  sendButton.addEventListener("click", (result) => {
+  sendButton.addEventListener("click", async (result) => {
     result.preventDefault(); // Prevent form submission
-    
-    try {
-        await 
-    } catch (e) {
 
+    try {
+      await Promise.all([
+        validateName(userName.value),
+        validateEmail(userEmail.value),
+        validateMessage(userMessage.value),
+      ]);
+      alert("Message sent!");
+      location.reload();
+    } catch (e) {
+      console.log(e);
+      alert(e);
+      intro.innerText = "If you'd like, just email mrharoon@berkeley.edu to contact me!";
+      if (e.includes("name")) {
+        pleaseName.removeAttribute("hidden");
+      } else {
+        pleaseName.setAttribute("hidden", true);
+      }
+      if (e.includes("email")) {
+        pleaseEmail.removeAttribute("hidden");
+      } else {
+        pleaseEmail.setAttribute("hidden", true);
+      }
+      if (e.includes("message")) {
+        pleaseMessage.removeAttribute("hidden");
+      } else {
+        pleaseMessage.setAttribute("hidden", true);
+      }
     }
   });
 };
