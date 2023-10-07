1e8738286518de9d70feb48af548cd7d2eb982bd
mrharoon-at-berkeley <mrharoon@berkeley.edu>
2023-10-07 10:42:04 -0700

    added error contact

--- contactpage/script.js
+++ contactpage/script.js
@@ -1,49 +1,59 @@
 const submit = () => {
-    // get info from name, email, and content item tags
-    // attempt to send via email
-    const userName = document.getElementById("name");
-    const userEmail = document.getElementById("email");
-    const userContent = document.getElementById("content");
-    const sendButton = document.getElementById("sendButton");
+  const userName = document.getElementById("name");
+  const userEmail = document.getElementById("email");
+  const userContent = document.getElementById("content");
+  const sendButton = document.getElementById("sendButton");
+  const intro = document.getElementById("intro");
+  const pleaseName = document.getElementById("pleaseName");
+  const pleaseEmail = document.getElementById("pleaseEmail");
+  const pleaseMessage = document.getElementById("pleaseMessage");
 
-    sendButton.addEventListener("click", (result) => {
-        result.preventDefault(); // Prevent form submission
-            alertMessage = "";
-            if (userName.value === "") {
-                alertMessage+= "ERROR: Missing name. Please give a name.\n";
-            }
-            if (userEmail.value ==="") {
-                alertMessage+= "ERROR: Missing email. Please give an email.\n";
-            } else {
-                const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-                if (!emailRegex.test(userEmail.value)) {
-                    alertMessage+= "ERROR: Invalid email. Please give a VALID email.\n";
-                }
-            }
-            if (userContent.value==="") {
-                alertMessage+= "ERROR: Missing message. Please write something under \"What's up?\"\n";
-            }
-
-            if (alertMessage !=="") {
-                alert(alertMessage);
-            } else {
-                alert("Message Sent!")
-                location.reload();
-            }
-            
-        
-        
-    });
-    
+  sendButton.addEventListener("click", (result) => {
+    result.preventDefault(); // Prevent form submission
+    alertMessage = "";
+    if (userName.value === "") {
+      alertMessage += "ERROR: Missing name. Please give a name.\n";
+      pleaseName.removeAttribute("hidden");
+    } else {
+      pleaseName.setAttribute("hidden");
+    }
+    if (userEmail.value === "") {
+      alertMessage += "ERROR: Missing email. Please give an email.\n";
+      pleaseEmail.removeAttribute("hidden");
+    } else {
+      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      if (!emailRegex.test(userEmail.value)) {
+        alertMessage += "ERROR: Invalid email. Please give a VALID email.\n";
+        pleaseName.removeAttribute("hidden");
+      } else {
+        pleaseName.setAttribute("hidden");
+      }
+    }
+    if (userContent.value === "") {
+      alertMessage +=
+        'ERROR: Missing message. Please write something under "What\'s up?"\n';
+      pleaseMessage.removeAttribute("hidden");
+    } else {
+      pleaseMessage.setAttribute("hidden");
+    }
+    if (alertMessage !== "") {
+      alert(alertMessage);
+      intro.innerText =
+        "If you'd like, just email mrharoon@berkeley.edu to contact me!";
+    } else {
+      alert("Message Sent!");
+      location.reload();
+    }
+  });
 };
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    // We wrap each question in a try-catch so that way even if
-    // your code for one question errors, the other ones
-    // will work.
-    try {
-      submit();
-    } catch (e) {
-      console.error(e);
-    }
-  });
+  // We wrap each question in a try-catch so that way even if
+  // your code for one question errors, the other ones
+  // will work.
+  try {
+    submit();
+  } catch (e) {
+    console.error(e);
+  }
+});
